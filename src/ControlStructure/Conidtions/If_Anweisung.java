7b9a7e6db0012e1970d97111a9e8e8848b81d098
Adnan Nassar <39063471+adnannassar@users.noreply.github.com>
2023-11-25 12:21:58 +0100

    init commit

---
+++ src/ControlStructure/Conidtions/If_Anweisung.java
@@ -0,0 +1,15 @@
+package ControlStructure.Conidtions;
+
+public class If_Anweisung {
+    public static void main(String[] args) {
+        int x = 1;
+        if (x > 0) {
+            System.out.println("X ist Positive!");
+        } else if (x < 0) {
+            System.out.println("X ist Negative!");
+        }
+        else {
+            System.out.println("X ist 0");
+        }
+    }
+}
