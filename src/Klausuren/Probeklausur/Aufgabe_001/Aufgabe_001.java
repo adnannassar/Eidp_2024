b79b800f3b9f95ef36d9b7bfb5ad4349d0ad948b
adnan <adnan.nassar90@gmail.com>
2024-02-11 17:12:48 +0100

    added: Multiple Choices

--- src/Klausuren/Probeklausur/Aufgabe_001/Aufgabe_001.java
+++ src/Klausuren/Probeklausur/Aufgabe_001/Aufgabe_001.java
@@ -1,12 +1,12 @@
 package Klausuren.Probeklausur.Aufgabe_001;
 
 public class Aufgabe_001 {
-    // 1- a:     b:     c:     d:
-    // 2- a:     b:     c:     d:
-    // 3- a:     b:     c:     d:
-    // 4- a:     b:     c:     d:
-    // 5- a:     b:     c:     d:
-    // 6- a:     b:     c:     d:
-    // 7- a:     b:     c:     d:
+    // 1- a:false     b:false     c:true      d:true
+    // 2- a:true      b:false     c:true      d:false
+    // 3- a:true      b:true      c:true      d:false
+    // 4- a:true      b:false     c:false     d:true
+    // 5- a:true      b:false     c:false     d:false
+    // 6- a:false     b:false     c:false     d:true
+    // 7- a:false     b:true      c:false     d:true
 }
 
